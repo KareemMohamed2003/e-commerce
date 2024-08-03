941ac1e42e9c318875e7e82e94552de736f02656
KareemMohamed2003 <sktbaker123@gmail.com>
2024-08-03 07:21:07 +0200

    Update appLoader.ts

--- src/appLoader.ts
+++ src/appLoader.ts
@@ -3,6 +3,7 @@ import { LoaderFunctionArgs } from "react-router-dom";
 import { redirect } from "react-router-dom";
 import { store } from "./Redux/reduxStore";
 
+
 export function loader({ request, params }: LoaderFunctionArgs) {
    console.log(store.getState())
    const state = {}
